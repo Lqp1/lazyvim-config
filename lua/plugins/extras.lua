3a24316be747a1f456e9e545c1e1f55025ee93d1
Thomas Langé <thomas.lange.oss@gmail.com>
2026-01-14 21:23:40 +0100

    Add perplexity connector to CodeCompanion config

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -24,6 +24,25 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("codecompanion").setup({
+        adapters = {
+          http = {
+            perplexity = function()
+              return require("codecompanion.adapters").extend("openai_compatible", {
+                name = "perplexity",
+                env = {
+                  url = "https://api.perplexity.ai", -- optional: default value is ollama url http://127.0.0.1:11434
+                  api_key = "cmd:cat ~/.perplexity",
+                  chat_url = "/chat/completions",
+                },
+                schema = {
+                  model = {
+                    default = "sonar-reasoning-pro", -- define llm model to be used
+                  },
+                },
+              })
+            end,
+          },
+        },
         interactions = {
           chat = { adapter = "copilot" },
           inline = { adapter = "copilot" },
